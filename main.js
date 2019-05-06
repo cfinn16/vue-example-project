4a70f0adb4b0842faa253350dacfd5d0c9c7e916
cfinn16 <connorfinnegan16@gmail.com>
2019-05-05 21:03:04 -0400

    conditional rendering for on sale

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = new Vue({
     product: "Socks",
     description: "A pair of warm, fuzzy socks",
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-    link: 'https://www.google.com/'
+    link: 'https://www.google.com/',
+    inventory: 100,
+    onSale: false
   }
 })
