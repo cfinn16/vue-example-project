c9e5c3a368821941b5d58726d05c5f216bf840ed
cfinn16 <connorfinnegan16@gmail.com>
2019-05-05 21:21:32 -0400

    iterating over detail and size lists

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var app = new Vue({
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.google.com/',
     inventory: 100,
-    onSale: false
+    onSale: false,
+    details: ["80 cotton", "20% polyester", "Gender-neutral"],
+    sizes: ["Small", "Medium", "Large"]
   }
 })
