368c77b6f735e39234c3a840f1a16631535e3f11
cfinn16 <connorfinnegan16@gmail.com>
2019-05-05 20:52:37 -0400

    added style and link

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: "Socks"
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    link: 'https://www.google.com/'
   }
 })
